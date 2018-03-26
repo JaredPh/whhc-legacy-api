011b97b4f464ea154d58a2019941453f490379bd
Jared Phayer <mail@jared.ph>
2018-03-26 20:37:27 +0100

    Removed all routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
-import { ClubModule } from './routes/club/club.module';
-import { TeamModule } from './routes/team/team.module';
-import { LocationModule } from './routes/location/location.module';
-import { GameModule } from './routes/game/game.module';
 
 @Module({
   imports: [
-      ClubModule,
-      GameModule,
-      LocationModule,
-      TeamModule,
       TypeOrmModule.forRoot(),
   ],
   controllers: [
