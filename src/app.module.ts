414531c12cb72563e61c43846a681577065d50d3
Jared Phayer <mail@jared.ph>
2018-04-07 00:30:15 +0100

    PR review

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
 import { MembersModule } from './routes/members/members.module';
 import { SessionModule } from './routes/session/session.module';
+
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
