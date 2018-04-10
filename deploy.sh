4561cfafe4ab9bd0d46a575fb04ef86c9a22b238
Jared Phayer <mail@jared.ph>
2018-04-11 00:41:02 +0100

    Python Update

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ docker --version
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 eval $(aws ecr get-login --region eu-west-1) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
+aws ecr get-login --no-include-email --region eu-west-1
 docker build -t whhc/api .
 docker tag whhc/api:latest 519891319653.dkr.ecr.eu-west-1.amazonaws.com/whhc/api:latest
 docker push 519891319653.dkr.ecr.eu-west-1.amazonaws.com/whhc/api:latest
